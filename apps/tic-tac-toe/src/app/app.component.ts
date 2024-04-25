8587c441d5a88495c47ee7f65318e79302b71e1c
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-04-25 18:14:32 +0530

    currentDisplayName$

--- apps/tic-tac-toe/src/app/app.component.ts
+++ apps/tic-tac-toe/src/app/app.component.ts
@@ -1,15 +1,21 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { BoardComponent } from '@boardgames-monorepo/gameboard';
+import { BreakpointObserverUtil } from '@boardgames-monorepo/utils';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, BoardComponent],
+  imports: [RouterModule, AsyncPipe, BoardComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent extends BreakpointObserverUtil {
   title = 'tic-tac-toe';
+
+  constructor() {
+    super();
+  }
 }
