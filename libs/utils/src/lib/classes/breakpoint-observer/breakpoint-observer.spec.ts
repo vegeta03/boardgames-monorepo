0614cc5c39a0a2e32da19e50f9a593010fa03c69
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-04-25 18:10:56 +0530

    BreakpointObserverUtil

---
+++ libs/utils/src/lib/classes/breakpoint-observer/breakpoint-observer.spec.ts
@@ -0,0 +1,7 @@
+import { BreakpointObserverUtil } from './breakpoint-observer';
+
+describe('BreakpointObserver', () => {
+  it('should create an instance', () => {
+    expect(new BreakpointObserverUtil()).toBeTruthy();
+  });
+});
